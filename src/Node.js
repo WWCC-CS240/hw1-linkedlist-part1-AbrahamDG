3c7dcf07542448b50d4f4c8773af89b785a6fbc1
AbrahamDG <151337793+AbrahamDG@users.noreply.github.com>
2025-01-15 17:53:00 +0000

    Finished Homework 1, Fixed test

--- src/Node.js
+++ src/Node.js
@@ -1,4 +1,22 @@
 class Node {
+
+    //first we need to construct the constructor
+    constructor(value, next = null){
+        this.value = value
+        this.next = next
+    }
+    
+    //Set the next node in the list 
+    setTheNextNodeList (node){
+        if (node instanceof Node || node === null){
+            this.next = node;
+        }
+    }
+
+    //get next node in the list
+    getNextNode(){
+        return this.next;
+    }
 }
 
 module.exports = Node;
